3d190784173064ef3c1f231f8e681a240e319dc5
narinaresh <narinaresh0302@gmail.com>
2023-10-22 16:42:10 +0530

    updated version of pervious code

--- ip_address_changer.py
+++ ip_address_changer.py
@@ -1,10 +1,42 @@
-
+import optparse
 import subprocess
-a="eth0"
-c = "180"
-d = 1
-for i in range(0,255,1):
-	d = d+1
-	str(d)
-	for j in range(0,255,1):
-		subprocess.call("ifconfig " + a + " inet " + "192.168." + c+"."+ str(d), shell= True)
+import time
+
+arguments = optparse.OptionParser()
+
+arguments.add_option("-i", "--interface", dest='interface',
+                     help="type of interface [example:eth0,wlan0..]")
+arguments.add_option("-t", "--time", dest="time_of_delay",
+                     help="the time delay between IP changes [in seconds] ")
+
+option = arguments.parse_args()
+
+interface = input("interface > ")
+
+time_of_delay = int(
+    input("Enter the time delay between IP changes (in seconds) > "))
+
+print(" ")
+print("---------------------------------------------------------")
+print("[+] Starting up")
+print("[+] Changing your ip address of", interface , " in every " + str(time_of_delay) + " sec" )
+print("---------------------------------------------------------")
+print(" ")
+last_digit = 0
+third_digit = 100
+
+while True:
+
+    ipaddr = f"192.168.{third_digit}.{last_digit}"
+    cmd = (["ifconfig", interface, "inet", ipaddr])
+    subprocess.call(cmd)
+
+    time.sleep(time_of_delay)
+    last_digit += 1
+    third_digit += 1
+
+    if third_digit > 255:
+        third_digit = 0
+
+    if last_digit > 255:
+        last_digit = 0
